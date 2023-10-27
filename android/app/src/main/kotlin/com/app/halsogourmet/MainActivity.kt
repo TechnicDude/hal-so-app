9327fd6619e50d02c64388b023f1d576c49499cf
Deepika Mourya <76613972+TechnicDude@users.noreply.github.com>
2023-10-27 21:49:44 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/app/halsogourmet/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.halsogourmet.app
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
+
+
+
+//package com.app.halsogourmet
+
+//import io.flutter.embedding.android.FlutterActivity
+
+//class MainActivity: FlutterActivity() {
+// }
